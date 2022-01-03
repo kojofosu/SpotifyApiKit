4b4ba62005c53cb19668a1b1499af46a4aaf4a37
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2022-01-03 11:01:15 +0000

    Add maven wrapper

--- src/main/java/model/Restriction.kt
+++ src/main/java/model/Restriction.kt
@@ -6,9 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Included in the response when a content restriction is applied. See [Restriction Object](https://developer.spotify.com/documentation/web-api/reference/#/) for more details.*/
 data class Restriction(
     /**
-     * reason
-    string
-    The reason for the restriction.
+     * The reason for the restriction.
     Albums may be restricted if the content is not available in a given market, to the user's subscription type,
     or when the user's account is set to not play explicit content. Additional reasons may be added in the future.
 
